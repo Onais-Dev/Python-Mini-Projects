02fd051411e7dcd790d084ec8bad3a0e7e70aece
Onais-Dev <onais.dev@gmail.com>
2024-11-08 06:20:44 +0500

    Doing some course

--- course.py
+++ course.py
@@ -1,1 +1,34 @@
-compound = True or False and not True or False
+# compound = True or False and not True or False
+
+# Conditional Statements
+# name = input("Name: ")
+
+# if name == "Onais":
+#     print("Hello,", name)
+
+# number = float(input("Enter a number: "))
+
+# if number > 0 and number % 2 == 0:
+#     print("This is a Postive Even Number")
+#     number2 = float(input("Enter another Number: "))
+#     if number2 < 0:
+#         print("This is Negative")
+#     else :
+#         print("This is positive")\
+
+# x = 5
+
+# result = "OK " if x >= 5 else "Not ok"
+# print(result)
+
+# list
+
+# x = [1,2,3,True, "abc"]
+# x.append("last element")
+# x.append(2)
+# print(len(x))
+
+name = input("Enter your name: ")
+print(name.count("$"))
+
+
